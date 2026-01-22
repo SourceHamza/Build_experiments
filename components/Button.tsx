dcab8d252891af270052fb7b3086a39914c7d882
HamzaSource <mohammed885hamza@gmail.com>
2026-01-22 19:40:16 +0530

    added motion

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,33 +1,47 @@
+"use client"
+
+import { motion } from "motion/react"
 import { ReactNode } from "react"
-
-type ButtonProp = { 
-    text : String , 
-    variant? : "primary" | "long"|  "secondary", 
-    icon? : ReactNode
+type ButtonProp = {
+    text: String,
+    variant?: "primary" | "long" | "secondary",
+    icon?: ReactNode
 
 }
 
 
-const variants = {  
-        primary : `bg-[#0D00FF] px-4 py-2 text-white` , 
-        long : `bg-[#0D00FF]  px-44 py-3 text-white` , 
-        secondary : `bg-[#E6E6E6] px-4 py-2 outline-1 outline-black text-black`, 
+const variants = {
+    primary: `bg-[#0D00FF] px-4 py-2 text-white`,
+    long: `bg-[#0D00FF]  px-44 py-3 text-white`,
+    secondary: `bg-[#E6E6E6] px-4 py-2 outline-1 outline-black text-black`,
 }
-const textVariants = {  
-        primary : `text-white text-[16px] ` , 
-        long : ` text-white text-[16px]` , 
-        secondary : `text-[#262626] text-[16px]`, 
+const textVariants = {
+    primary: `text-white text-[16px] `,
+    long: ` text-white text-[16px]`,
+    secondary: `text-[#262626] text-[16px]`,
 }
 
 
 
-export function Button({text,variant,icon} :ButtonProp){ 
-    return ( 
-        <div className={`flex gap-3 font-main items-center justify-center  rounded-[3px] ${variant? variants[variant] : variants["primary"]}`}  >
-            {icon}
-            <p className={`${variant? textVariants[variant] : textVariants["primary"] }`}>
+export function Button({ text, variant, icon }: ButtonProp) {
+    return (
+        <motion.div className={`flex gap-3 cursor-pointer font-main items-center justify-center  rounded-[3px] ${variant ? variants[variant] : variants["primary"]}`}
+            whileHover={{ scale: 1.01 }}
+            whileTap={{ scale: 0.99 }} >
+
+            <motion.div
+                animate={{ y: [0, -2, 0]  , x:[-1,2,-1]}}
+                transition={{
+                    duration: 2,
+                    repeat: Infinity,
+                    ease: "easeInOut",
+                }}
+            >
+                {icon}
+            </motion.div>
+            <p className={`${variant ? textVariants[variant] : textVariants["primary"]}`}>
                 {text}
             </p>
-        </div>
+        </motion.div>
     )
 }
--- components/input.tsx
+++ components/input.tsx
@@ -1,16 +1,23 @@
-import { div } from "motion/react-client"
+"use client"
+
 import { Mail } from "@/components/assets/Mail"
+import { motion } from "motion/react"
 
 
-
-export function Input() { 
+export function Input() {
     return (
         <div className=" outline-1 outline-black px-4 py-3 font-main text-[14px]  rounded-[5px]  shadow-[0px_4px_0px_0px_rgba(0,_0,_0,_0.25)] flex items-center gap-3">
-            <Mail/>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5 }}
+            >
+                <Mail />
+            </motion.div>
             <input type="text" className="focus:outline-none w-108"
-            placeholder="you@example.com" />
+                placeholder="you@example.com" />
 
-            
+
         </div>
     )
 }
